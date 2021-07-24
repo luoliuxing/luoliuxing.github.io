bfc014e438fda1320fbe7f7d3f6fbfd26be0979a
luoliuxing <83436373+luoliuxing@users.noreply.github.com>
2021-07-24 15:26:37 +0800

    first add files
    first add files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//$(document).ready(function(){
+//	
+//	$(".d1").hover(
+//	    function(){
+//	        $("img").fadeIn("slow");
+//			$("img").css({"float": "left",
+//		    	"margin-left": "70px",
+//		    	"margin-top": "70px"});
+//	    },
+//	    function(){
+//	        $("img").fadeOut("slow");
+//				
+//	    }
+//	);
+//});
